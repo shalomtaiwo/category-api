51e7e06037dbf3a8e2aab981b25a56d51dda4ddd
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2022-12-14 14:41:55 +0200

    fix text

---
+++ api/category.php
@@ -0,0 +1,6 @@
+<?php
+include('../template.php');
+
+echo '<pre>';
+
+print_r(json_encode(getAllCategory()));
---
+++ api/product.php
@@ -0,0 +1,6 @@
+<?php
+include('../template.php');
+
+$category = $_GET['category'];
+echo '<pre>';
+print_r(json_encode(getProductsInCategory($category)));
